e0f142f6ec0b5c59f1af902cf234e51ff8035f11
LI-YONG-QI <a2369028@gmail.com>
2022-10-25 09:55:50 +0800

    feat: add homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [people, setPeople] = useState(0);
   return (
-    <div className="App">
-      <main className="birthday-info">
-        <h3>{people} birthday today</h3>
-        <button className="clean-btn">Clean All</button>
-      </main>
-    </div>
+    <main>
+      <Link to="/birthday">Go to Birthday</Link>
+    </main>
   );
 }
 
