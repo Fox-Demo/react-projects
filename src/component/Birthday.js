791f7a6f08dc39f376f07bdcf7eea946e03426cb
LI-YONG-QI <a2369028@gmail.com>
2022-10-25 09:56:56 +0800

    feat: add homepage

---
+++ src/component/Birthday.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./Birthday.css";
+
+function Birthday() {
+  const [people, setPeople] = useState(0);
+  return (
+    <div className="birthday-main">
+      <main className="birthday-info">
+        <h3>{people} birthday today</h3>
+        <button className="clean-btn">Clean All</button>
+      </main>
+    </div>
+  );
+}
+
+export default Birthday;
