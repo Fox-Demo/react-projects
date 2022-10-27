30efe42ea20864985ec3534678c29fbfb3ea3844
LI-YONG-QI <a2369028@gmail.com>
2022-10-27 21:49:45 +0800

    feat: 10/27 Complete Birthday project

---
+++ src/component/birthday/Birthday.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./Birthday.css";
+import Data from "./Data";
+
+function Birthday() {
+  const [people, setPeople] = useState(Data);
+  return (
+    <div className="birthday-main">
+      <main className="birthday-people">
+        <h3 className="birthday-title">{people.length} birthday today</h3>
+        {people.map((index) => {
+          return (
+            <div className="person">
+              <img src={index.image} width="75px" height="75px"></img>
+              <section>
+                <p className="name">{index.name}</p>
+                <p className="age">{index.age} years</p>
+              </section>
+            </div>
+          );
+        })}
+        <button
+          className="clean-btn"
+          onClick={() => {
+            setPeople([]);
+          }}
+        >
+          Clean All
+        </button>
+      </main>
+    </div>
+  );
+}
+
+export default Birthday;
