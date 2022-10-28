3ae2b49ca2e907a7f2142b62902fba02f3c3fff6
LI-YONG-QI <a2369028@gmail.com>
2022-10-28 15:19:39 +0800

    feat: Make tour project
    Missing loading and background color had a bug

--- src/component/tour/Tour.js
+++ src/component/tour/Tour.js
@@ -16,20 +16,39 @@ export const Tour = () => {
   }, []);
   return (
     <div className="tour-container">
-      <article className="tour-title">
+      <header className="tour-header">
         <h1>Our Tours</h1>
         <div className="underline"></div>
-      </article>
+      </header>
+
       <main className="tour-main">
         {tours.map((tour) => {
           return (
-            <figure className="tour-info">
+            <figure key={tour.id} className="tour-info">
               <img src={tour.image} />
               <figcaption>
-                <h2>{tour.name}</h2>
-                <p>{tour.price}</p>
-                <p>{tour.info}</p>
-                <button>Not interested</button>
+                <article className="tour-title">
+                  <h4>{tour.name} </h4>
+                  <h4 className="price">{"$" + tour.price}</h4>
+                </article>
+                <article className="tour-describe">
+                  <p>
+                    {tour.info.substring(0, 200) + "... "}
+                    <button className="read-btn">Read More</button>
+                  </p>
+                </article>
+                <button
+                  className="interested-btn"
+                  onClick={() => {
+                    const result = tours.filter((index) => {
+                      return index.id !== tour.id;
+                    });
+
+                    setTours(result);
+                  }}
+                >
+                  Not Interested
+                </button>
               </figcaption>
             </figure>
           );
